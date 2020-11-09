1e9de789e66256e8481f1ed255174d9cdf3f4854
supermario1990 <406517780@qq.com>
2020-11-09 18:14:17 +0800

    update

---
+++ example/go_rpc/client/client.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/rpc"
+)
+
+type Args struct {
+	A, B int
+}
+
+type Quotient struct {
+	Quo, Rem int
+}
+
+func main() {
+	client, err := rpc.DialHTTP("tcp", "127.0.0.1:1234")
+	if err != nil {
+		log.Fatal("dialing:", err)
+	}
+
+	for {
+		// Synchronous call
+		args := &Args{7, 8}
+		var reply int
+		err = client.Call("Arith.Multiply", args, &reply)
+		if err != nil {
+			fmt.Println("arith error:", err)
+		}
+		fmt.Printf("Arith: %d*%d=%d\n", args.A, args.B, reply)
+
+		// Asynchronous call
+		quotient := new(Quotient)
+		divCall := client.Go("Arith.Divide", args, quotient, nil)
+		replyCall := <-divCall.Done // will be equal to divCall
+		if replyCall.Error != nil {
+			fmt.Println("arith error:", replyCall.Error)
+		}
+		fmt.Printf("Arith: %d/%d=%d...%d", args.A, args.B, quotient.Quo, quotient.Rem)
+		// check errors, print, etc.
+	}
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/supermario1990/gnet_client
 go 1.15
 
 require (
+	github.com/chriswalz/bit v0.9.10 // indirect
 	github.com/panjf2000/gnet v1.3.0
 	github.com/petermattis/goid v0.0.0-20180202154549-b0b1615b78e5 // indirect
 	github.com/sasha-s/go-deadlock v0.2.0
